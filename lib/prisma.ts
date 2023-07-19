5ee71c38912b7299c9edc2039d99d4d44b35e2c8
Filip Rusoł <filadakrzy@gmail.com>
2023-07-19 21:58:56 +0200

    asd

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,4 +5,4 @@ const globalForPrisma = global as unknown as {prisma: PrismaClient | undefined};
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ log: ['query'] });
 
 if (process.env.NODE_ENV !== 'production')
-    globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma
